0518b25aadbc6eaf0893e329ca780e58dae8ccd9
bradyxiao <bradyxiao@tencent.com>
2018-11-02 22:10:18 +0800

    add files

---
+++ COSXML/Model/Bucket/GetBucketCORSResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using COSXML.Model.CORSTag;
+/**
+* Copyright (c) 2018 Tencent Cloud. All rights reserved.
+* 11/2/2018 10:01:37 PM
+* bradyxiao
+*/
+namespace COSXML.Model.Bucket
+{
+    public class GetBucketCORSResult : CosResult
+    {
+        public CORSConfiguration corsConfiguration;
+
+        public override void ParseResponse(Network.Response response)
+        {
+            base.ParseResponse(response);
+        }
+
+        public override string GetResultInfo()
+        {
+            return base.GetResultInfo() + (corsConfiguration == null ? "" : "\n " + corsConfiguration.GetInfo());
+        }
+
+    }
+}
---
+++ COSXML/Model/Tag/CORSConfiguration.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/**
+* Copyright (c) 2018 Tencent Cloud. All rights reserved.
+* 11/2/2018 10:02:19 PM
+* bradyxiao
+*/
+namespace COSXML.Model.CORSTag
+{
+    public sealed class CORSConfiguration
+    {
+        public List<CORSRule> corsRules;
+
+        public String GetInfo()
+        {
+            StringBuilder stringBuilder = new StringBuilder("{CORSConfiguration:\n");
+            if(corsRules != null){
+                foreach(CORSRule corsRule in corsRules)
+                {
+                    if (corsRule != null) stringBuilder.Append(corsRule.GetInfo()).Append("\n");
+                }
+            }
+            stringBuilder.Append("}");
+            return stringBuilder.ToString();
+        }
+
+    }
+
+    public sealed class CORSRule{
+        public String id;
+        public String allowedOrigin;
+        public List<String> allowedMethods;
+        public List<String> allowedHeaders;
+        public List<String> exposeHeaders;
+        public int maxAgeSeconds;
+     
+        public String GetInfo()
+        {
+            StringBuilder stringBuilder = new StringBuilder("{CORSRule:\n");
+            stringBuilder.Append("ID:").Append(id).Append("\n");
+            stringBuilder.Append("AllowedOrigin:").Append(allowedOrigin).Append("\n");
+            if (allowedMethods != null)
+            {
+                foreach (String method in allowedMethods)
+                {
+                    if(method != null)stringBuilder.Append("AllowedMethod:").Append(method).Append("\n");
+                }
+            }
+            if (allowedHeaders != null)
+            {
+                foreach (String header in allowedHeaders)
+                {
+                    if(header != null)stringBuilder.Append("AllowedHeader:").Append(header).Append("\n");
+                }
+            }
+            if (exposeHeaders != null)
+            {
+                foreach (String exposeHeader in exposeHeaders)
+                {
+                    if(exposeHeader != null)stringBuilder.Append("ExposeHeader:").Append(exposeHeader).Append("\n");
+                }
+            }
+            stringBuilder.Append("MaxAgeSeconds:").Append(maxAgeSeconds).Append("\n");
+            stringBuilder.Append("}");
+            return stringBuilder.ToString();
+        }
+    }
+}
